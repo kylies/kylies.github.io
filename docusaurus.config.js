cb8b7badefbada8da87ebe6e371f6ab43c82f4e8
Kylie <kszymesko@gmail.com>
2024-03-01 17:00:34 -0500

    WAP 9 images

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon2.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
